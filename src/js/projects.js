4894886156d42b18f5c5c1f64e75b54482460ec9
tommybommy29 ryan <trya5786@uni.sydney.edu.au>
2023-02-28 19:21:14 +1100

    project page overhauled!

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,37 +1,25 @@
 class BorderGenerator {
-    constructor() {
-      this.cards = document.querySelectorAll(`.project`);
-      this.generateTails();
-      this.setRandomColors();
-      setInterval(() => {
-        this.setRandomColors();
-      }, 7000);
-    }
-  
-    generateTails() {
-      this.cards.forEach((card) => {
-        [
-          `top`,
-          `right`,
-          `bottom`,
-          `left`,
-        ].forEach((side) => {
-          const tail = document.createElement(`div`);
-          tail.classList.add(`tail`, side);
-          card.appendChild(tail);
-        });
-      });
-    }
-  
-    setRandomColors() {
-      this.cards.forEach((card) => {
-        if (!card.style.getPropertyValue(`--color1`)) {
-          card.style.setProperty(`--color1`, `hsl(${Math.random() * 360}, 100%, 50%)`);
-          card.style.setProperty(`--color2`, `hsl(${Math.random() * 360}, 100%, 50%)`);
-        }
-      });
-    }
+  constructor() {
+    this.projects = document.querySelectorAll(`.project`);
+    this.generateTails();
+    console.log("BorderGenerator generated")
   }
-  
-  const borderGenerator = new BorderGenerator();
-  
+
+  generateTails() {
+    this.projects.forEach((project) => {
+      [
+        `top`,
+        `right`,
+        `bottom`,
+        `left`,
+      ].forEach((side) => {
+        const tail = document.createElement(`div`);
+        tail.classList.add(`tail`, side);
+        project.appendChild(tail);
+      });
+    });
+  }
+
+}
+
+export default BorderGenerator;
