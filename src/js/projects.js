919b898b08ccfe3986ebdbf880f697fc75042856
tommybommy29 ryan <trya5786@uni.sydney.edu.au>
2023-03-05 14:41:00 +1100

    finished work experience on projects page

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,6 +3,9 @@ class BorderGenerator {
     this.projects = document.querySelectorAll(`.project`);
     this.generateTails();
     console.log("BorderGenerator generated")
+
+    // Call the method to initialize the collapsible list
+    this.initCollapsibleList();
   }
 
   generateTails() {
@@ -20,6 +23,31 @@ class BorderGenerator {
     });
   }
 
+  initCollapsibleList() {
+    // Get all the collapsible items and loop through them
+    const collapsibleItems = document.querySelectorAll('.collapsible');
+    collapsibleItems.forEach((item) => {
+      // Set the first item to be shown
+      const content = item.querySelector('.collapsible-content');
+
+      // Add click event listener to toggle the display of additional items
+      const toggleButton = item.querySelector('.collapsible-toggle');
+      const arrow = item.querySelector('.list-down-arrow');
+      toggleButton.addEventListener('click', () => { 
+        
+        if (content.classList.contains("open")) {
+          content.classList.remove("open");
+          arrow.classList.remove("list-arrow-rotate");
+          content.style.maxHeight = null;
+        } else {
+          content.classList.add("open");
+          arrow.classList.add('list-arrow-rotate');
+          content.style.maxHeight = content.scrollHeight + 'px';
+        }
+        console.log(content.classList.toString());
+      });
+    });
+  }
 }
 
 export default BorderGenerator;
